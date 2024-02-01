67c9fc581a9ed126d6bf7e2585302930731041d6
ChuBox <dedalboyet16@gmail.com>
2024-02-01 10:32:44 +0800

    dsfsfds

--- otherf/connection-db.php
+++ otherf/connection-db.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = new mysqli('23.106.126.222', 'dfoiwidm_student_profiles', 'student_profiles123', 'dfoiwidm_student_profiles');
+$con = new mysqli('s527', 'dfoiwidm_student_profiles', 'student_profiles123', 'dfoiwidm_student_profiles');
 
 // if ($con->connect_error){
 //     die("Connection Failed: " . $con->connect_error);
