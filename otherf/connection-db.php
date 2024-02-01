f2209c6c5dce1b69a78288a965b88dbf906fdf0c
ChuBox <dedalboyet16@gmail.com>
2024-02-01 13:36:56 +0800

    hahaha

--- otherf/connection-db.php
+++ otherf/connection-db.php
@@ -7,9 +7,9 @@ $dbname = 'dfoiwidm_student_profiles';
 
 $con = new mysqli($servername,$username,$password, $dbname);
 
-// if ($con->connect_error){
-//     die("Connection Failed: " . $con->connect_error);
-// }
+if ($con->connect_error){
+    die("Connection Failed: " . $con->connect_error);
+}
 
 
 
