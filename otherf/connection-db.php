4dd632cdc8095a862b853eac7d0b0b8c16126766
ChuBox <dedalboyet16@gmail.com>
2024-02-01 13:25:49 +0800

    hudhwd

--- otherf/connection-db.php
+++ otherf/connection-db.php
@@ -1,6 +1,11 @@
 <?php
 
-$con = new mysqli('dfoiwidm', 'dfoiwidm_student_profiles', 'stdprofilesystem', 'dfoiwidm_student_profiles');
+$servername = 'dfoiwidm';
+$username = 'dfoiwidm_student_profiles';
+$password = 'stdprofilesystem';
+$dbname = 'dfoiwidm_student_profiles';
+
+$con = new mysqli($servername,$username,$password, $dbname);
 
 // if ($con->connect_error){
 //     die("Connection Failed: " . $con->connect_error);
